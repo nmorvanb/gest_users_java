4b1e35a625f5ff64662059d0317f606c5c3b7f5d
mtanneau <mtanneau@B4-W7-80.sio.chaptalqper>
2019-03-07 16:34:00 +0100

    Suite

--- src/gest.java
+++ src/gest.java
@@ -10,6 +10,11 @@ public class gest extends JFrame implements ActionListener{
 	private ImageIcon imagef = new ImageIcon("images/fond.png");
 	private JLabel logo = new JLabel(image,JLabel.CENTER);
 	private JLabel fond = new JLabel(imagef, JLabel.CENTER);
+	private JButton deconnexion = new JButton("Deconnexion");
+	private JButton modifier = new JButton("Modifier");
+	private JButton consulter = new JButton("Consulter");
+	private JButton ajouter = new JButton("Ajouter");
+	private Font comic = new Font("Comic sans MS",Font.BOLD,18);
 	
 	public gest() {
 		this.setTitle("Gestion utilisateur");
@@ -23,7 +28,11 @@ public class gest extends JFrame implements ActionListener{
 		
 		logo.setBounds(0, 0, 318, 213);
 		
+		deconnexion.setFont(comic);
+		deconnexion.setBounds(700, 0, 100, 50);
+		
 		pan.add(logo);
+		pan.add(deconnexion);
 		pan.add(fond);
 		
 		this.setContentPane(pan);

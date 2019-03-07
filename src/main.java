952582437379bb2e6f99de3b3c8cccc26ab8cfed
nmorvanb <nmorvanb@B4-W7-90.sio.chaptalqper>
2019-03-07 16:27:45 +0100

    Connexion a la base de donnée

--- src/main.java
+++ src/main.java
@@ -2,12 +2,29 @@ import javax.print.attribute.standard.JobOriginatingUserName;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.sql.DriverManager;
+import java.sql.*;
 
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Stub de la méthode généré automatiquement
 
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection cnx = DriverManager.getConnection("jdbc:mysql://localhost/gsb_frais","ts1","ts1");
+			System.out.println("BDD fonctionnelle");
+		}
+		catch(ClassNotFoundException e)
+		{
+			System.out.println("Erreur Driver");
+		}
+		catch(SQLException e)
+		{
+			System.out.println("Erreur  Database");
+		}
+			
+		
 		accueil f1 = new accueil(); 
 	}
 

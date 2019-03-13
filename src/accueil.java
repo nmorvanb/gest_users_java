c4d65e2d4cad878a099406b89c36f4a96ed7c43f
maeltanneau <tanneaumael@gmail.com>
2019-03-13 15:03:35 +0100

    Modif

--- src/accueil.java
+++ src/accueil.java
@@ -4,69 +4,71 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class accueil extends JFrame implements ActionListener {
-	
-	private JPanel pan = new JPanel();
-	private ImageIcon image = new ImageIcon("images/gsblogo.png");
-	private ImageIcon imagef = new ImageIcon("images/fond.png");
-	private JLabel logo = new JLabel(image,JLabel.CENTER);
-	private JLabel fond = new JLabel(imagef, JLabel.CENTER);
-	private JLabel userLabel = new JLabel("Nom d'utilisateur");
-	private JLabel mdpLabel = new JLabel("Mot de passe");
-	private JTextField user = new JTextField();
-	private JTextField mdp = new JTextField();
-	private JButton connexion = new JButton("Connexion");
-	private Font comic = new Font("Comic sans MS",Font.BOLD,18);
-	private Font comic2 = new Font("Comic sans MS",Font.BOLD,15);
-	
-	public accueil() {
-		this.setTitle("Accueil");
-		this.setSize(800,800);
-		this.setLocationRelativeTo(null);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		pan.setLayout(null);
-		
-		fond.setBounds(0,0,800,800);
-		
-		logo.setBounds(230, 50, 318, 213);
-		
-		userLabel.setFont(comic);
-		userLabel.setBounds(230, 300, 318, 150);
-		
-		user.setFont(comic2);
-		user.setForeground(Color.red);
-		user.setBounds(230,400,318,25);
-		
-		mdpLabel.setFont(comic);
-		mdpLabel.setBounds(230, 400, 318, 150);
-		
-		mdp.setFont(comic2);
-		mdp.setForeground(Color.red);
-		mdp.setBounds(230,500,318,25);
-		
-		connexion.setFont(comic);
-		connexion.setBounds(309, 600, 159, 50);
-		connexion.addActionListener(this);
-		
-		
-		pan.add(logo);
-		pan.add(userLabel);
-		pan.add(user);
-		pan.add(mdpLabel);
-		pan.add(mdp);
-		pan.add(connexion);
-		pan.add(fond);
-		
-		this.setContentPane(pan);
-		this.setVisible(true);
-	}
+        
+        private JPanel pan = new JPanel();
+        private ImageIcon image = new ImageIcon("images/gsblogo.png");
+        private ImageIcon imagef = new ImageIcon("images/fond.png");
+        private JLabel logo = new JLabel(image,JLabel.CENTER);
+        private JLabel fond = new JLabel(imagef, JLabel.CENTER);
+        private JLabel userLabel = new JLabel("Nom d'utilisateur");
+        private JLabel mdpLabel = new JLabel("Mot de passe");
+        private JTextField user = new JTextField();
+        private JTextField mdp = new JTextField();
+        private JButton connexion = new JButton("Connexion");
+        private Font comic = new Font("Comic sans MS",Font.BOLD,18);
+        private Font comic2 = new Font("Comic sans MS",Font.BOLD,15);
+        
+        public accueil() {
+                this.setTitle("Accueil");
+                this.setSize(800,800);
+                this.setLocationRelativeTo(null);
+                this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                
+                pan.setLayout(null);
+                
+                fond.setBounds(0,0,800,800);
+                
+                logo.setBounds(230, 50, 318, 213);
+                
+                userLabel.setFont(comic);
+                userLabel.setBounds(230, 300, 318, 150);
+                
+                user.setFont(comic2);
+                user.setForeground(Color.red);
+                user.setBounds(230,400,318,25);
+                
+                mdpLabel.setFont(comic);
+                mdpLabel.setBounds(230, 400, 318, 150);
+                
+                mdp.setFont(comic2);
+                mdp.setForeground(Color.red);
+                mdp.setBounds(230,500,318,25);
+                
+                connexion.setFont(comic);
+                connexion.setBounds(309, 600, 159, 50);
+                connexion.addActionListener(this);
+                
+                
+                pan.add(logo);
+                pan.add(userLabel);
+                pan.add(user);
+                pan.add(mdpLabel);
+                pan.add(mdp);
+                pan.add(connexion);
+                pan.add(fond);
+                
+                this.setContentPane(pan);
+                this.setVisible(true);
+        }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if (e.getSource()== connexion){
-			this.setVisible(false);
-			gest f1 = new gest();
-		}
-		
-	}
+        @Override
+        public void actionPerformed(ActionEvent e) {
+                if (e.getSource()== connexion){
+                        
+                        this.setVisible(false);
+                        gest f1 = new gest();
+                }
+                
+        }
 }
+
--- src/gest.java
+++ src/gest.java
@@ -5,45 +5,62 @@ import java.awt.event.*;
 
 public class gest extends JFrame implements ActionListener{
 
-	private JPanel pan = new JPanel();
-	private ImageIcon image = new ImageIcon("images/gsblogo.png");
-	private ImageIcon imagef = new ImageIcon("images/fond.png");
-	private JLabel logo = new JLabel(image,JLabel.CENTER);
-	private JLabel fond = new JLabel(imagef, JLabel.CENTER);
-	private JButton deconnexion = new JButton("Deconnexion");
-	private JButton modifier = new JButton("Modifier");
-	private JButton consulter = new JButton("Consulter");
-	private JButton ajouter = new JButton("Ajouter");
-	private Font comic = new Font("Comic sans MS",Font.BOLD,18);
-	
-	public gest() {
-		this.setTitle("Gestion utilisateur");
-		this.setSize(800,800);
-		this.setLocationRelativeTo(null);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		pan.setLayout(null);
-		
-		fond.setBounds(0,0,800,800);
-		
-		logo.setBounds(0, 0, 318, 213);
-		
-		deconnexion.setFont(comic);
-		deconnexion.setBounds(600, 0, 200, 50);
-		
-		pan.add(logo);
-		pan.add(deconnexion);
-		pan.add(fond);
-		
-		this.setContentPane(pan);
-		this.setVisible(true);
-	}
+        private JPanel pan = new JPanel();
+        private ImageIcon image = new ImageIcon("images/gsblogo.png");
+        private ImageIcon imagef = new ImageIcon("images/fond.png");
+        private JLabel logo = new JLabel(image,JLabel.CENTER);
+        private JLabel fond = new JLabel(imagef, JLabel.CENTER);
+        private JButton deconnexion = new JButton("Deconnexion");
+        private JButton modifier = new JButton("Modifier");
+        private JButton consulter = new JButton("Consulter");
+        private JButton ajouter = new JButton("Ajouter");
+        private Font comic = new Font("Comic sans MS",Font.BOLD,18);
+        
+        public gest() {
+                this.setTitle("Gestion utilisateur");
+                this.setSize(800,800);
+                this.setLocationRelativeTo(null);
+                this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                
+                pan.setLayout(null);
+                
+                fond.setBounds(0,0,800,800);
+                
+                logo.setBounds(0, 0, 318, 213);
+                
+                deconnexion.setFont(comic);
+                deconnexion.setBounds(635, 0, 150, 50);
+                deconnexion.addActionListener(this);
+                
+                modifier.setFont(comic);
+                modifier.setBounds(300, 300, 200, 50);
+                modifier.addActionListener(this);
+                
+                consulter.setFont(comic);
+                consulter.setBounds(300, 400, 200, 50);
+                consulter.addActionListener(this);
+                
+                ajouter.setFont(comic);
+                ajouter.setBounds(300, 500, 200, 50);
+                ajouter.addActionListener(this);
+                
+                pan.add(logo);
+                pan.add(deconnexion);
+                pan.add(modifier);
+                pan.add(consulter);
+                pan.add(ajouter);
+                pan.add(fond);
+                
+                this.setContentPane(pan);
+                this.setVisible(true);
+        }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		
-	}
-
-
-	
+        @Override
+        public void actionPerformed(ActionEvent e) {
+                if (e.getSource()== deconnexion){
+                        
+                        this.setVisible(false);
+                        accueil f1 = new accueil();
+                }
+        }
 }
